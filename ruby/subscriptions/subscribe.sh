b93a63ce0f3b156395b5d678b22c247d56c5d988
Avi Flax <avi.flax@fundingcircle.com>
2018-04-23 17:50:59 -0400

    Reorg so as to support multiple Ruby toolsets
    Right now we have 3 scripts related to subscriptions; I think of that as
    a single toolset for subscriptions. I’m about to add a new toolset for
    working with repos, and I want to reduce duplication between these two
    toolsets. So although I’m not thrilled to group toolsets by programming
    system, it feels like the pragmatic thing to do here.

--- ruby/subscriptions/subscribe.sh
+++ ruby/subscriptions/subscribe.sh
@@ -2,7 +2,11 @@
 
 set -e
 
+cp ../Dockerfile ../*.rb ./
+
 IMAGE_ID=`docker build --quiet .`
 
 # The -i is required for the progress bar to work
 docker run -i --rm -e "ORG=$ORG" -e "TOKEN=$TOKEN" $IMAGE_ID ruby _subscribe.rb $1
+
+rm Dockerfile config.rb
