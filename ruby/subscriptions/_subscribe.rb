ddf1626261d28b88df1ed403a353d7d39f876574
Avi Flax <avi.flax@fundingcircle.com>
2018-04-25 12:21:17 -0400

    Use Kernel#warn and Kernel#abort
    To improve the code and to make the program more compliant with UNIX
    conventions.
    
    As per feedback from @sgerrand in #1 — Thanks Sasha!

--- ruby/subscriptions/_subscribe.rb
+++ ruby/subscriptions/_subscribe.rb
@@ -6,16 +6,11 @@ require_relative './config'
 
 THREADS = 6
 
-def oh_no(msg)
-  puts msg
-  exit false
-end
-
 def args
   topic = ARGV[0] || ''
 
   if topic.empty?
-    oh_no 'The first arg must be either a topic or - to indicate to read a list of repos via STDIN.'
+    abort 'The first arg must be a topic, or - to indicate to read a list of repos via STDIN.'
   end
 
   read_from_stdin = topic == '-'
@@ -25,7 +20,7 @@ end
 
 def repos_from_stdin
   repo_names = STDIN.readlines chomp: true
-  oh_no 'If the first arg is - you must supply a list of repos via STDIN.' if repo_names.empty?
+  abort 'If the first arg is - you must supply a list of repos via STDIN.' if repo_names.empty?
   repo_names
 end
 
@@ -35,17 +30,17 @@ def print_now(s)
 end
 
 def get_repo_names(topic, org, client)
-  print "Retrieving list of #{org} repos with topic #{topic} ... "
+  print_now "Retrieving list of #{org} repos with topic #{topic} ... "
 
   result = client.search_repos "user:#{org} topic:#{topic}"
 
-  puts "#{result.total_count} repos found\n\n"
+  print_now "#{result.total_count} repos found\n"
 
   # TODO: possible bug in this function: I don’t know whether auto_paginate works for this method.
   # So if there’s more than 100 repos with the given topic this might return only the first 100.
   # This is an edge case so I’m OK with this for now.
   if result.total_count == 100
-    puts "Warning! There may be additional matching repos that weren’t retrieved!\n\n"
+    warn "Warning! There may be additional matching repos that weren’t retrieved!\n"
   end
 
   result.items.map(&:name)
@@ -65,18 +60,18 @@ end
 
 def lets_do_this
   Config.validate!
-  org = Config[:org]
   client = Config.make_client
   topic, read_from_stdin = args
 
-  repo_names = read_from_stdin ? repos_from_stdin : get_repo_names(topic, org, client)
+  repo_names = read_from_stdin ? repos_from_stdin : get_repo_names(topic, Config[:org], client)
 
   if repo_names.empty?
-    puts 'No repos, nothing to do.'
+    sleep 0.01 # prevent a race condition between output written to stdout and to stderr
+    warn 'No repos, nothing to do.'
     exit true
   end
 
-  subscribe repo_names, org, client
+  subscribe repo_names, Config[:org], client
 end
 
 lets_do_this
