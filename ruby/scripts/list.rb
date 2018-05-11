97554ecb6543eaf56ef69e75f424bb86ec470665
Avi Flax <avi.flax@fundingcircle.com>
2018-05-11 15:03:03 -0400

    Add support for --sources to list repos

--- ruby/scripts/list.rb
+++ ruby/scripts/list.rb
@@ -11,11 +11,15 @@ kind = ARGV[0]         # Kind of thing to list. Must be 'repos' (for now)
 first_flag = ARGV[1]   # Must be --all | --private | --subscribed | --topic
 topic = ARGV[2]        # Required if first_flag is --topic; otherwise should be nil/blank
 
-forks_note = 'note: --forks lists repos that were forked INTO the specified org'
+notes = {
+  forks: 'note: --forks lists repos that were forked INTO the specified org',
+  # As far as I can tell, `sources` is not limited to repos that *have* forks
+  sources: 'note: --sources lists repos that are not forks'
+}
 
 usage = 'usage: list repos ' \
-        "<one of: (--all | --private | --public | --subscribed | --forks | --topic <topic>)> \n" \
-        " #{forks_note}"
+        "<one of: (--all | --private | --public | --subscribed | --forks | --sources | --topic <topic>)> \n" \
+        " #{notes[:forks]} \n #{notes[:sources]}"
 
 criterion = first_flag&.[](2..-1)&.downcase&.to_sym
 
@@ -25,14 +29,14 @@ if kind != 'repos' || !first_flag&.start_with?('--') || criterion.nil? ||
   abort usage
 end
 
-warn forks_note if criterion == :forks
+warn notes[criterion] if notes.include? criterion
 
 repos = case criterion
         when :subscribed
           GitHubTools.subscribed_repos org, client
         when :topic
           GitHubTools.org_repos org, client, topic: topic
-        when :all, :private, :public, :forks
+        when :all, :private, :public, :forks, :sources
           GitHubTools.org_repos org, client, type: criterion.to_s
         else
           abort usage
