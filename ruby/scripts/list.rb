0592d02ea52ce35a58b3ad9d32edff921e61752e
Avi Flax <avi.flax@fundingcircle.com>
2018-05-15 16:33:18 -0400

    Improve CLI option validation and code readability

--- ruby/scripts/list.rb
+++ ruby/scripts/list.rb
@@ -7,10 +7,6 @@ Config.validate!
 client = Config.make_client
 org = Config.fetch(:github_org).strip
 
-kind = ARGV[0]         # Kind of thing to list. Must be 'repos' (for now)
-first_flag = ARGV[1]   # See usage below
-topic = ARGV[2]        # Required if first_flag is --topic; otherwise should be nil/blank
-
 notes = {
   forks: 'lists repos that were forked INTO the specified org',
   # As far as I can tell, `sources` is not limited to repos that *have* forks (as I had hoped)
@@ -30,12 +26,20 @@ usage = <<~HEREDOC
       --topic <topic>
 HEREDOC
 
-# This is ridiculous -- we should probably be using OptionParser.
+# I tried OptionParser, I swear, but it doesn’t seem to support required arguments nor a case
+# wherein at least one of a set of options is required.
+kind, first_flag, arg_val = ARGV
 criterion = first_flag&.[](2..-1)&.downcase&.sub('-', '')&.to_sym
+
+case criterion
+when :forksof then repo_name = arg_val
+when :topic then topic = arg_val
+end
 
 if kind != 'repos' || !first_flag&.start_with?('--') || criterion.nil? ||
    (criterion == :topic && (topic.nil? || topic.empty?)) ||
-   (criterion != :topic && !(topic.nil? || topic.empty?))
+   (criterion == :forksof && (repo_name.nil? || repo_name.empty?)) ||
+   (!%i[topic forksof].include(:criterion) && !(arg_val.nil? || arg_val.empty?))
   abort usage
 end
 
@@ -48,7 +52,7 @@ repos =
   when :topic
     GitHubTools.org_repos org, client, topic: topic
   when :forksof
-    raise NotImplementedError
+    raise NotImplementedError, "Cannot yet not list forks of #{repo_name}"
   when :all, :private, :public, :forks, :sources
     GitHubTools.org_repos org, client, type: criterion.to_s
   else
