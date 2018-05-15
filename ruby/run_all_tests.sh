71aac238a4e0cd47cc0db3819cc793f76ed6342b
Avi Flax <avi.flax@fundingcircle.com>
2018-05-15 17:54:38 -0400

    Make run_all_tests.sh more portable
    The docker image we’re using in CircleCI uses Alpine Linux, so it
    doesn’t have bash installed. Sure, we could install bash, but that’d
    slow the builds down or require me to understand caching on CircleCI.
    
    So instead I’m changing this script to be more portable across POSIX
    shells.

--- ruby/run_all_tests.sh
+++ ruby/run_all_tests.sh
@@ -1,10 +1,16 @@
-#!/bin/bash
+#!/bin/sh
+
+# This script needs to work in Alpine Linux without bash.
 
 set -e
 
 # This script is meant to be run from the root of the repo, but this enables it
 # to work from within `ruby` as well:
-if [[ `pwd` != *"/ruby" ]]; then cd ruby; fi
+# I found the suggestion to use case here: https://stackoverflow.com/a/2830416/7012
+case `pwd` in
+  *ruby*) echo "" ;;
+  *)      cd ruby ;;
+esac
 
 # The -i is here just in case someone inserts an interactive debugging
 #   breakpoint into any of the code under test (or the tests).
