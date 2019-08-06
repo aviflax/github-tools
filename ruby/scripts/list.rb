d6c9db273c46b0181fc7fdfda6ef41ac0d251956
Avi Flax <avi.flax@fundingcircle.com>
2019-08-06 12:54:11 +0100

    Code tweak for readability
    Replace dashes with underscores instead of simply removing them.

--- ruby/scripts/list.rb
+++ ruby/scripts/list.rb
@@ -29,17 +29,17 @@ HEREDOC
 # I tried OptionParser, I swear, but it doesn’t seem to support required arguments nor a case
 # wherein at least one of a set of options is required.
 kind, first_flag, arg_val = ARGV
-criterion = first_flag&.[](2..-1)&.downcase&.sub('-', '')&.to_sym
+criterion = first_flag&.[](2..-1)&.downcase&.sub('-', '_')&.to_sym
 
 case criterion
-when :forksof then repo_name = arg_val
+when :forks_of then repo_name = arg_val
 when :topic then topic = arg_val
 end
 
 if kind != 'repos' || !first_flag&.start_with?('--') || criterion.nil? ||
    (criterion == :topic && (topic.nil? || topic.empty?)) ||
-   (criterion == :forksof && (repo_name.nil? || repo_name.empty?)) ||
-   (!%i[topic forksof].include?(criterion) && !(arg_val.nil? || arg_val.empty?))
+   (criterion == :forks_of && (repo_name.nil? || repo_name.empty?)) ||
+   (!%i[topic forks_of].include?(criterion) && !(arg_val.nil? || arg_val.empty?))
   abort usage
 end
 
@@ -51,7 +51,7 @@ repos =
     GitHubTools.subscribed_repos org, client
   when :topic
     GitHubTools.org_repos org, client, topic: topic
-  when :forksof
+  when :forks_of
     raise NotImplementedError, "Cannot yet not list forks of #{repo_name}"
   when :all, :private, :public, :forks, :sources
     GitHubTools.org_repos org, client, type: criterion.to_s
