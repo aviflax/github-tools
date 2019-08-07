e4153328445da914ed31b8b7f3758bcaf3140cb5
Avi Flax <avi.flax@fundingcircle.com>
2019-08-07 16:47:22 +0100

    WIP++

--- clojure/src/ghr/io/repos.clj
+++ clojure/src/ghr/io/repos.clj
@@ -11,14 +11,8 @@
 (defn org-repos-for-topic
   "Returns a (possibly empty) sequential collection of maps that represent repositories, or throws."
   [org-name topic]
-  (let [result (ts/search-repos nil {:user org-name
-                                     :topic topic})]
-    (when (:incomplete_results result)
-      (warn "Results may be incomplete as the query exceeded the GitHub API’s internal timeout."))
-    (when (> (:total_count result) 100)
-      (warn "Results ARE INCOMPLETE as there are more than 100 results and this tool does not"
-            "currently return more than the first 100."))
-    (:items result)))
+  (:items (ts/search-repos nil {:user org-name :topic topic}
+                               {:all-pages true})))
 
 (defn owner-name
   [repo]
@@ -33,8 +27,7 @@
   org to which the user is subscribed (watching), or throws."
   [org-name user-name]
   (filter (fn [repo] (owned-by? repo org-name))
-          ;; TODO: what about pagination?
-          (tr/watching user-name)))
+          (tr/watching user-name {:all-pages true})))
 
 (defn has-codeowners?
   [repo]
