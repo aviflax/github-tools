f742ab67d387751d7b4d8be960145a41c1d27ba4
Avi Flax <avi.flax@fundingcircle.com>
2019-08-28 12:51:08 -0400

    [skipci] WIP++ add a few simple tests. No I/O tests yet.

--- clojure/test/ghot/io/cli/main_test.clj
+++ clojure/test/ghot/io/cli/main_test.clj
@@ -1,1 +1,16 @@
-(ns ghot.io.cli.main-test)
+(ns ghot.io.cli.main-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [ghot.io.cli :refer [exit-on-exit?]]
+            [ghot.io.cli.main :as main])
+  (:import [clojure.lang ExceptionInfo]))
+
+(reset! exit-on-exit? false)
+
+(deftest -main
+  (testing "sad paths"
+    (is (thrown? ExceptionInfo (main/-main)))
+    (is (thrown? ExceptionInfo (main/-main "foo")))
+    (is (thrown? ExceptionInfo (main/-main "list")))
+    (is (thrown? ExceptionInfo (main/-main "listrepos")))
+    (is (thrown? ExceptionInfo (main/-main "list-repos")))
+    (is (thrown? ExceptionInfo (main/-main "list users")))))
