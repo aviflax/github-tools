db0ef39c329c6985c26e497d979f31b991689f3d
Avi Flax <avi.flax@fundingcircle.com>
2019-08-29 16:27:57 -0400

    WIP++ minor test improvements

--- clojure/test/ghot/repos_test.clj
+++ clojure/test/ghot/repos_test.clj
@@ -3,4 +3,8 @@
             [ghot.repos :as gr]))
 
 (deftest owner-username
-  (is (= "Optimus Prime" (gr/owner-username {:owner {:login "Optimus Prime"}}))))
+  (is (= "Optimus Prime" (gr/owner-username {:owner {:login "Optimus Prime"}})))
+  (is (= "" (gr/owner-username {:owner {:login ""}})))
+  (is (nil? (gr/owner-username {:owner {}})))
+  (is (nil? (gr/owner-username {})))
+  (is (nil? (gr/owner-username nil))))
